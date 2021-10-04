80577a8abe1bfaf95379f23e62a5427727a3dcaf
freelancer-q <zhang.he@qodash.com>
2021-10-04 00:56:12 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  extends: [
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+  overrides: [
+    {
+      files: ["scripts/**"],
+      rules: { "no-process-exit": "off" },
+    },
+    {
+      files: ["hardhat.config.ts", "scripts/**", "test/**"],
+      rules: {
+        "node/no-unpublished-import": "off",
+        "node/no-unsupported-features/es-syntax": "off",
+      },
+    },
+  ],
+};
